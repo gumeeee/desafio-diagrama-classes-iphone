c405012b1c6b143f613e6e988a1e690235f07341
gumeeee <gaxco123@gmail.com>
2023-10-22 16:11:35 -0300

    Projeto finalizado

---
+++ README.md
@@ -0,0 +1,7 @@
+# Diagramação de classes do iPhone
+
+## Diagramação em UML
+
+<p align="center">
+  <img src="./assets/modelagem.jpg" alt="Diagrama">
+</p>
---
+++ src/iPhone.java
@@ -0,0 +1,40 @@
+package src;
+
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    public void ligar(String numero) {
+        // Implemente o código aqui
+    }
+
+    public void atender(String numero) {
+        // Implemente o código aqui
+    }
+
+    public void iniciarCorreioVoz(String mensagem, String destinatario) {
+        // Implemente o código aqui
+    }
+
+    public void exibirPagina(String url) {
+        // Implemente o código aqui
+    }
+
+    public void adicionarNovaAba(String url) {
+        // Implemente o código aqui
+    }
+
+    public void atualizarPagina() {
+        // Implemente o código aqui
+    }
+
+    public void tocar() {
+        // Implemente o código aqui
+    }
+
+    public void pausar() {
+        // Implemente o código aqui
+    }
+
+    public void selecionarMusica() {
+        // Implemente o código aqui
+    }
+}
